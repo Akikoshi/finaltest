4b169c58ca1873125a5bf239585696c13a607f9f
Akikoshi <Theise80@gmail.com>
2016-12-16 20:18:54 +0100

    second

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+namespace Heiset\Finaltest;
+session_start();
+require_once (__DIR__ . '/../vendor/autoload.php');
+new Init();
---
+++ src/Init.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 16.12.2016
+ * Time: 20:13
+ */
+
+namespace Heiset\Finaltest;
+
+
+class Init
+{
+    public function __construct()
+    {
+        echo "hallo welt";
+    }
+}
